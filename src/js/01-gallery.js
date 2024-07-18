3dffb94c9587367b50b0d71aa3181f2110fc04ad
AlexandraPlaton16 <platonmariaalexandra@yahoo.com>
2024-07-18 20:11:59 +0200

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Descris în documentație
+import SimpleLightbox from 'simplelightbox';
+// Import suplimentar de stil
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+function initializeGallery() {
+  const galleryContainer = document.querySelector('.gallery');
+  const galleryMarkup = createGalleryMarkup(galleryItems);
+  galleryContainer.innerHTML = galleryMarkup;
+  initializeLightbox();
+}
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </li>
+    `;
+    })
+    .join('');
+}
+
+function initializeLightbox() {
+  new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+}
+
+initializeGallery();
